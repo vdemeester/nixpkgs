446c57271d29481498de50ed38236a1ac65cb4fc
Doron Behar <doron.behar@gmail.com>
2025-12-30 13:50:02 +0200

    python3Packages.gputil: init at 1.4.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6476,6 +6476,8 @@ self: super: with self; {
 
   gpustat = callPackage ../development/python-modules/gpustat { };
 
+  gputil = callPackage ../development/python-modules/gputil { };
+
   gpxpy = callPackage ../development/python-modules/gpxpy { };
 
   gpytorch = callPackage ../development/python-modules/gpytorch { };
